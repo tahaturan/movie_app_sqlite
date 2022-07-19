cbf7b26b9ebb95d0f2bbae45c87292717ddb1d98
Taha Turan <tahaaturann16@gmail.com>
2022-07-19 03:24:21 +0300

    Started

---
+++ lib/views/movie_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MovieDetailPage extends StatelessWidget {
+  const MovieDetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Film Ismi'),
+      ),
+      body: Container(),
+    );
+  }
+}
---
+++ lib/views/movie_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MoviePage extends StatefulWidget {
+  const MoviePage({Key? key}) : super(key: key);
+
+  @override
+  State<MoviePage> createState() => _MoviePageState();
+}
+
+class _MoviePageState extends State<MoviePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Filmler'),
+      ),
+      body: Container(),
+    );
+  }
+}
